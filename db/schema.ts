7a6cc54c577e90d6dfb05d4c4b6baf5f5c5ce3b5
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2024-03-22 20:16:42 -0300

    setup drizzle and turso db

---
+++ db/schema.ts
@@ -0,0 +1,7 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const courses = sqliteTable("courses", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+});
---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
+  },
+} satisfies Config;
