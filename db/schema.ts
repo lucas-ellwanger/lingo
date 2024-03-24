10b9ac14c8671f89ea1dcefb5d860dc1595110cc
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2024-03-24 20:36:04 -0300

    add tables and relations into the db schema

--- db/schema.ts
+++ db/schema.ts
@@ -9,7 +9,112 @@ export const courses = sqliteTable("courses", {
 
 export const coursesRelations = relations(courses, ({ many }) => ({
   userProgress: many(userProgress),
+  units: many(units),
 }));
+
+export const units = sqliteTable("units", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  courseId: integer("course_id")
+    .references(() => courses.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  position: integer("position").notNull(),
+});
+
+export const unitsRelations = relations(units, ({ one, many }) => ({
+  course: one(courses, {
+    fields: [units.courseId],
+    references: [courses.id],
+  }),
+  lessons: many(lessons),
+}));
+
+export const lessons = sqliteTable("lessons", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  unitId: integer("unit_id")
+    .references(() => units.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  position: integer("position").notNull(),
+});
+
+export const lessonsRelations = relations(lessons, ({ one, many }) => ({
+  unit: one(units, {
+    fields: [lessons.unitId],
+    references: [units.id],
+  }),
+  challenges: many(challenges),
+}));
+
+export const challenges = sqliteTable("challenges", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  lessonId: integer("lesson_id")
+    .references(() => lessons.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  type: text("type", { enum: ["SELECT", "ASSIST"] }).notNull(),
+  question: text("question").notNull(),
+  position: integer("position").notNull(),
+});
+
+export const challengesRelations = relations(challenges, ({ one, many }) => ({
+  lesson: one(lessons, {
+    fields: [challenges.lessonId],
+    references: [lessons.id],
+  }),
+  challengeOptions: many(challengeOptions),
+  challengeProgress: many(challengeProgress),
+}));
+
+export const challengeOptions = sqliteTable("challenge_options", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  challengeId: integer("challenge_id")
+    .references(() => challenges.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  text: text("question").notNull(),
+  correct: integer("correct", { mode: "boolean" }).notNull(),
+  imageSrc: text("image_src"),
+  audioSrc: text("audio_src"),
+});
+
+export const challengeOptionsRelations = relations(
+  challengeOptions,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeOptions.challengeId],
+      references: [challenges.id],
+    }),
+  }),
+);
+
+export const challengeProgress = sqliteTable("challenge_progress", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  userId: text("user_id").notNull(),
+  challengeId: integer("challenge_id")
+    .references(() => challenges.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  completed: integer("completed", { mode: "boolean" }).default(false).notNull(),
+});
+
+export const challengeProgressRelations = relations(
+  challengeProgress,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeProgress.challengeId],
+      references: [challenges.id],
+    }),
+  }),
+);
 
 export const userProgress = sqliteTable("user_progress", {
   userId: text("user_id").primaryKey(),
@@ -30,4 +135,9 @@ export const userProgressRelations = relations(userProgress, ({ one }) => ({
 }));
 
 export type Course = typeof courses.$inferSelect;
+export type Unit = typeof units.$inferSelect;
+export type Lesson = typeof lessons.$inferSelect;
+export type Challenge = typeof challenges.$inferSelect;
+export type ChallengeOption = typeof challengeOptions.$inferSelect;
+export type ChallengeProgress = typeof challengeProgress.$inferSelect;
 export type UserProgress = typeof userProgress.$inferSelect;
